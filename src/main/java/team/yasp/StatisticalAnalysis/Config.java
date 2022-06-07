9c4b299cf2695daff1230aa32639af362f9cf88a
dmitry <dmitry.mikhailov@devim.team>
2022-06-07 18:08:22 +0300

    update

---
+++ src/main/java/team/yasp/StatisticalAnalysis/Config.java
@@ -0,0 +1,7 @@
+package team.yasp.StatisticalAnalysis;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+}
---
+++ src/main/java/team/yasp/StatisticalAnalysis/Controller.java
@@ -0,0 +1,7 @@
+package team.yasp.StatisticalAnalysis;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+@org.springframework.stereotype.Controller
+@RequestMapping
+public class Controller {
+}
