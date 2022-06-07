262a314ef96be8ac0aa82bb1596e5e96234a17f4
dmitry <dmitry.mikhailov@devim.team>
2022-06-07 18:08:22 +0300

    update

---
+++ src/main/java/team/yasp/StatisticalAnalysis/StatisticalAnalysisApplication.java
@@ -0,0 +1,13 @@
+package team.yasp.StatisticalAnalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticalAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticalAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/yasp/StatisticalAnalysis/StatisticalAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package team.yasp.StatisticalAnalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticalAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
