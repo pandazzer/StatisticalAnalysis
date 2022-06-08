40f90b8e8ddcceb27a6497e497366e30911eb302
dmitry <dmitry.mikhailov@devim.team>
2022-06-08 23:27:38 +0300

    update

--- src/main/java/team/yasp/StatisticalAnalysis/StatisticalAnalysisApplication.java
+++ src/main/java/team/yasp/StatisticalAnalysis/StatisticalAnalysisApplication.java
@@ -1,12 +1,15 @@
 package team.yasp.StatisticalAnalysis;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StatisticalAnalysisApplication {
-
+static Logger log =  LogManager.getLogger();
 	public static void main(String[] args) {
+		log.info("start");
 		SpringApplication.run(StatisticalAnalysisApplication.class, args);
 	}
 
