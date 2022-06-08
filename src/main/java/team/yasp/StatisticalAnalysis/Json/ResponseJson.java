a5dd56c8a7907eb066637ed893449bf671270b23
dmitry <dmitry.mikhailov@devim.team>
2022-06-08 17:03:35 +0300

    update

--- src/main/java/team/yasp/StatisticalAnalysis/Json/ResponseJson.java
+++ src/main/java/team/yasp/StatisticalAnalysis/Json/ResponseJson.java
@@ -1,4 +1,37 @@
 package team.yasp.StatisticalAnalysis.Json;
 
 public class ResponseJson {
+    private String min;
+    private String max;
+    private String count;
+
+    public ResponseJson(String min, String max, String count) {
+        this.min = min;
+        this.max = max;
+        this.count = count;
+    }
+
+    public String getMin() {
+        return min;
+    }
+
+    public void setMin(String min) {
+        this.min = min;
+    }
+
+    public String getMax() {
+        return max;
+    }
+
+    public void setMax(String max) {
+        this.max = max;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
 }
